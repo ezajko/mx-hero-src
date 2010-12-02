5e9121e7c6ca75ffa2029612c3e82c7e7111d221
mmarmol <mmarmol@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2010-12-02 18:59:03 +0000

    CON-107 CON-109

--- trunk/console/backend/src/main/java/org/mxhero/console/backend/service/FeatureService.java
+++ trunk/console/backend/src/main/java/org/mxhero/console/backend/service/FeatureService.java
@@ -3,6 +3,7 @@ package org.mxhero.console.backend.service;
 import java.util.Collection;
 
 import org.mxhero.console.backend.vo.CategoryVO;
+import org.mxhero.console.backend.vo.FeatureRuleVO;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,4 +15,16 @@ public interface FeatureService {
 	
 	@Secured("ROLE_DOMAIN_ADMIN")
 	Collection<CategoryVO> findFeaturesByDomainId(Integer domainId);
+	
+	@Secured("ROLE_DOMAIN_ADMIN")
+	void remove(Integer id);
+	
+	@Secured("ROLE_DOMAIN_ADMIN")
+	void toggleStatus(Integer id);
+	
+	@Secured("ROLE_DOMAIN_ADMIN")
+	Collection<FeatureRuleVO> getRulesByDomainId(Integer domainId, Integer featureId);
+	
+	@Secured("ROLE_ADMIN")
+	Collection<FeatureRuleVO> getRulesWithoutDomain(Integer featureId);
 }
