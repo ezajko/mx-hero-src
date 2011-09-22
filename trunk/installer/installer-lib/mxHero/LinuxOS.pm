80466621f5ddb7d2097363fd8462e561d3fcc9f0
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-09-22 00:44:00 +0000

    Simple test for system Linux distribution.

---
+++ trunk/installer/installer-lib/mxHero/LinuxOS.pm
@@ -0,0 +1,38 @@
+package mxHero::LinuxOS;
+
+use strict;
+use warnings;
+
+# Super simple distri check
+sub getDistri
+{
+	# Detect: Ubuntu, Debian, Redhat, Suse
+	my $distri;
+	if ( -f "/etc/lsb-release" ) {
+		$distri = &_processLsbRelease();
+	} elsif ( -f "/etc/redhat-release" ) {
+		$distri = "Redhat";
+	} elsif ( -f "/etc/SuSE-release" ) {
+		$distri = "Suse";
+	}
+	
+	return $distri;
+}
+
+sub _processLsbRelease
+{
+	if ( ! open(F,"/etc/lsb-release") ) {
+		warn $!;
+	}
+
+	while (<F>) {
+		if ( $_ =~ /DISTRIB_ID=(\w+)/ ) {
+			return $1;
+		}
+	}
+
+}
+
+
+
+1;
