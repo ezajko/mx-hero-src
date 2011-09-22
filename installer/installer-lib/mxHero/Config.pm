4b0054ce4438633ca2b6c7c083d4a46160d798f5
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-09-22 01:19:28 +0000

    Added master.cf update routine.

--- installer/installer-lib/mxHero/Config.pm
+++ installer/installer-lib/mxHero/Config.pm
@@ -39,6 +39,10 @@ sub install
 							   print_me => T("Found postfix master.cf at")." [$file]." );
 			if ( $bool ) {
 				# set master.cf path for alteration routine
+				if ( ! &_alterPostfixMasterCf( $file, "127.0.0.1",5555,5556) ) {
+					$$error = T("Failed to alter ")."'$file'";
+					return 0;
+				}
 				last;
 			}
 		}
@@ -49,6 +53,10 @@ sub install
 		my $reply = $term->get_reply( prompt => T("Please enter full path to your master.cf".":"));
 		if ( $reply && -f $reply ) {
 			# set master.cf path for alteration routine
+			if ( ! &_alterPostfixMasterCf( $reply, "127.0.0.1",5555,5556) ) {
+				$$error = T("Failed to alter ")."'$reply'";
+				return 0;
+			}
 		} else {
 			print T("Failed to find file")." '$reply' \n";
 			print T("Stopping installation")."\n";
@@ -113,6 +121,97 @@ sub _addUpdateStartupScript
 
 	return 1;
 }
+
+sub _alterPostfixMasterCf {
+
+	my $file = $_[0];
+	my $ip = $_[1];
+	my $port1 = $_[2];
+	my $port2 = $_[3];
+
+	# Copy original file to .old or .old.1, .old.2 etc...
+	my $oldFile = $file.".old";
+	if ( -f $oldFile ) {
+		my $count = 1;
+		while ( -f $oldFile.".".$count ) {
+			$count++;
+		}
+
+		$oldFile = $oldFile.".".$count;
+	}
+
+	if ( ! copy($file,$oldFile) ) {
+		warn $!;
+		return 0;
+	}
+
+	if ( ! open (MASTER, $oldFile) || ! open (NEW, ">$file") ) {
+		warn $!;
+		return 0;
+	}
+
+	my $mxHero = <<END;
+# MXHERO ENTRY - START
+smtp      inet  n       -       -       -       -       smtpd
+	-o smtpd_proxy_filter=$ip:$port1
+	$ip:$port2 inet     n       -       n       -       -       smtpd
+	-o smtpd_authorized_xforward_hosts=$ip/24
+	-o smtpd_client_restrictions=
+	-o smtpd_helo_restrictions=
+	-o smtpd_sender_restrictions=
+	-o smtpd_recipient_restrictions=permit_mynetworks,reject
+	-o smtpd_data_restrictions=
+	-o mynetworks=$ip/24
+	-o receive_override_options=no_unknown_recipient_checks
+# MXHERO ENTRY - END
+END
+
+	my $line;
+	my $inSmtp;
+	my $foundSmtp;
+
+	while ($line = <MASTER>) {
+		if ($line =~ /^\s*$/ or $line =~ /^\s*\#/) {
+			# comment line or white space, write out
+			print NEW $line;
+			next;
+		}
+
+		if ($line =~ /^\w/) {
+			if ($line =~ /^smtp\s+inet\s+/) { # begin smtp logical line
+				$foundSmtp = 1;
+				$inSmtp = 1;
+				# prepend line with '#'
+				print NEW "# MXHERO comment out ...\n";
+				print NEW '# '.$line;
+			} elsif ($inSmtp) { # exiting smtp logical line
+				$inSmtp = 0;
+				# place mxHero smtp logical line
+				print NEW $mxHero;
+				print NEW $line;
+			} else {
+				print NEW $line;
+			}
+		} elsif ($line =~ /^\s+/) { # logical line continuation
+			if ($inSmtp) {
+				# prepend line with '#'
+				print NEW '# '.$line;
+			} else {
+				print NEW $line;
+			}
+		}
+	}
+
+	if ( ! $foundSmtp ) { # did not find a valid SMTP line, so add to end of file
+		print NEW $mxHero;
+	}
+
+	close MASTER;
+	close NEW;
+
+	return 1;
+}
+
 
 
 1;
