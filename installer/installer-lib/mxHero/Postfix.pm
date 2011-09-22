9848f92ee65589c9ba14d69cff13a979e6be1e09
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-09-22 14:34:15 +0000

    Added methods to configure.

--- installer/installer-lib/mxHero/Postfix.pm
+++ installer/installer-lib/mxHero/Postfix.pm
@@ -2,6 +2,10 @@ package mxHero::Postfix;
 
 use strict;
 use warnings;
+no warnings qw(uninitialized);
+
+use mxHero::Config;
+
 
 sub download
 {
@@ -10,6 +14,8 @@ sub download
 
 sub install
 {
+	# Don't install if postfix / Zimbra already exists.
+	
 	return;
 }
 
@@ -20,7 +26,184 @@ sub upgrade
 
 sub configure
 {
-	return;
+
+	my $error = $_[0];
+
+	# MASTER.CF
+	# List of possible master.cf file locations - good for Ubuntu, Debian, Redhat.
+	my @masterCfFiles = qw(/etc/postfix/master.cf /opt/zimbra/postfix/conf/master.cf);
+	my $term = Term::ReadLine->new( 'mxHero' );
+	my $bool;
+	
+	for my $file ( @masterCfFiles ) {
+		if ( -f $file ) {
+			$bool = $term->ask_yn( prompt => T("Alter this master.cf for mxHero?"),
+							   default  => 'y',
+							   print_me => T("Found postfix master.cf at")." [$file]." );
+			if ( $bool ) {
+				# set master.cf path for alteration routine
+				if ( ! &_alterPostfixMasterCf( $file, "127.0.0.1",5555,5556) ) {
+					$$error = T("Failed to alter ")."'$file'";
+					return 0;
+				}
+				last;
+			}
+		}
+	}
+	
+	if ( ! $bool ) {
+		print "\n".T("Did not find a postfix 'master.cf' file.")."\n";
+		my $reply = $term->get_reply( prompt => T("Please enter full path to your master.cf".":"));
+		if ( $reply && -f $reply ) {
+			# set master.cf path for alteration routine
+			if ( ! &_alterPostfixMasterCf( $reply, "127.0.0.1",5555,5556) ) {
+				$$error = T("Failed to alter ")."'$reply'";
+				return 0;
+			}
+		} else {
+			print T("Failed to find file")." '$reply' \n";
+			print T("Stopping installation")."\n";
+			$$error = T("Failed to find configuration file");
+			return 0;
+		}
+	}
+
+	# INIT SCRIPT
+	if ( ! &_addUpdateStartupScript() ) {
+		$$error = T("Failed to install startup script");
+		return 0;
+	}
+
+	return 1;
 }
+
+
+## PRIVATE SUBS
+
+sub _alterPostfixMasterCf {
+
+	my $file = $_[0];
+	my $ip = $_[1];
+	my $port1 = $_[2];
+	my $port2 = $_[3];
+
+	# Copy original file to .old or .old.1, .old.2 etc...
+	my $oldFile = $file.".old";
+	if ( -f $oldFile ) {
+		my $count = 1;
+		while ( -f $oldFile.".".$count ) {
+			$count++;
+		}
+
+		$oldFile = $oldFile.".".$count;
+	}
+
+	if ( ! copy($file,$oldFile) ) {
+		warn $!;
+		return 0;
+	}
+
+	if ( ! open (MASTER, $oldFile) || ! open (NEW, ">$file") ) {
+		warn $!;
+		return 0;
+	}
+
+	my $mxHero = <<END;
+# MXHERO ENTRY - START
+smtp      inet  n       -       -       -       -       smtpd
+	-o smtpd_proxy_filter=$ip:$port1
+	$ip:$port2 inet     n       -       n       -       -       smtpd
+	-o smtpd_authorized_xforward_hosts=$ip/24
+	-o smtpd_client_restrictions=
+	-o smtpd_helo_restrictions=
+	-o smtpd_sender_restrictions=
+	-o smtpd_recipient_restrictions=permit_mynetworks,reject
+	-o smtpd_data_restrictions=
+	-o mynetworks=$ip/24
+	-o receive_override_options=no_unknown_recipient_checks
+# MXHERO ENTRY - END
+END
+
+	my $line;
+	my $inSmtp;
+	my $wroteSmtp;
+
+	while ($line = <MASTER>) {
+		if ($line =~ /^\s*$/ or $line =~ /^\s*\#/) {
+			# comment line or white space, write out
+			print NEW $line;
+			next;
+		}
+
+		if ($line =~ /^\w/) {
+			if ($line =~ /^smtp\s+inet\s+/) { # begin smtp logical line
+				$inSmtp = 1;
+				# prepend line with '#'
+				print NEW "# MXHERO comment out ...\n";
+				print NEW '# '.$line;
+			} elsif ($inSmtp) { # exiting smtp logical line
+				$inSmtp = 0;
+				# place mxHero smtp logical line
+				print NEW $mxHero;
+				$wroteSmtp = 1;
+				print NEW $line;
+			} else {
+				print NEW $line;
+			}
+		} elsif ($line =~ /^\s+/) { # logical line continuation
+			if ($inSmtp) {
+				# prepend line with '#'
+				print NEW '# '.$line;
+			} else {
+				print NEW $line;
+			}
+		}
+	}
+
+	if ( ! $wroteSmtp ) { # did not find a valid SMTP line, so add to end of file
+		print NEW $mxHero;
+	}
+
+	close MASTER;
+	close NEW;
+
+	return 1;
+}
+
+
+# Discover what the init.d mecanism is and copy associated init file and do update-rc.d or whatever appropriate.
+sub _addUpdateStartupScript
+{
+	my $distri = &mxHero::LinuxOS::getDistri();
+	
+	if ( $distri eq "Ubuntu" || $distri eq "Debian" ) {
+		# TODO
+		# copy init and do update-rc.d
+		if ( ! copy ( "$myConfig{INSTALLER_PATH}/scripts/mxhero-init-debian_ubuntu", "/etc/init.d/mxhero" ) ) {
+			warn $!;
+			return 0;
+		}
+		
+		if ( ! chmod( 0755, '/etc/init.d/mxhero' ) ) {
+			warn "Failed to chmod /etc/init.d/mxhero";
+			return 0;
+		}
+		
+		system("/usr/sbin/update-rc.d mxhero defaults");
+		
+	} elsif ( $distri eq "Redhat" ) {
+		# TODO
+	} elsif ( $distri eq "Suse" ) {
+		# TODO
+	} else {
+		# could not identify Distribution
+		warn "Unknown Linux Distribution.";
+		return 0;
+	}
+
+	return 1;
+}
+
+
 
 1;
