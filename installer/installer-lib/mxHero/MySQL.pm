5d9d75005d2cf08eb4ebb0a9e5dbf88be11aeb66
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-09-23 20:54:00 +0000

    Ordering SQL files.

--- installer/installer-lib/mxHero/MySQL.pm
+++ installer/installer-lib/mxHero/MySQL.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 no warnings qw(uninitialized);
 
+use mxHero::Config;
 use mxHero::Tools;
 
 sub download
@@ -18,6 +19,8 @@ sub install
 
 sub upgrade
 {
+	my $error = $_[0];
+
 	# TODO
 	# Determine Version of mxHero installed
 	my $mxheroVersion = &mxHero::Tools::mxHeroVersion();
@@ -25,11 +28,17 @@ sub upgrade
 	my $installerVersion = &mxHero::Tools::mxHeroInstallerVersion();
 	# Sanity check
 	if ( ! $mxheroVersion || ! $installerVersion ) {
-		warn "mxHero Version information incomplete!";
+		$$error = "mxHero Version information incomplete.";
+		return 0;
 	}
-	# TODO: Step difference in version sql alter files from current to updated
 	# list sql files and order by version precedence
-
+	my @sqlFiles;
+	if ( @sqlFiles = &_versionOrderedSqlFiles( $mxheroVersion ) ) {
+		# TODO: execute SQL
+	} else {
+		$$error = "Failed to find SQL files.";
+		return 0;
+	}
 	
 	return 1;
 }
@@ -38,5 +47,56 @@ sub configure
 {
 	return 1;
 }
+
+
+## PRIVATE
+
+# returns ordered array of sql files - oldest first.
+# NOTE: IMPORTANT THAT VERSION NOT EXCEDE TWO DIGITS PER POINT LOCATION - EX. 99.99.99
+sub _versionOrderedSqlFiles
+{
+	my $mxheroVersion = $_[0];
+	
+	my %files;
+	
+	if ( ! opendir(DIR,"$myConfig{INSTALLER_PATH}/scripts/sql") ) {
+		warn $!;
+		return undef;
+	}
+	
+	my @files = grep ( /\d+\-\d+\-\d+\D*.*\.sql/, readdir( DIR ) );
+	
+	# Score installed version
+	my $mxHeroVersionScore;
+	if ( $mxheroVersion ) {
+		if ( $mxheroVersion =~ /^(\d+)\.(\d+)\.(\d+)\D*.*/ ) {
+			$score = ($1 * 10000) + ($2 * 100) + $3;
+		} else {
+			warn "mxHero version incorrect format: $mxheroVersion";
+			return 0;
+		}
+	}
+	
+	# Score file versions
+	while my $file ( @files ) {
+		my $score;
+		if ( $file =~ /(\d+)\-(\d+)\-(\d+)\D*.*\.sql/ ) {
+			$score = ($1 * 10000) + ($2 * 100) + $3;
+			# Only SQL after the current installed version
+			if ( $score > $mxHeroVersionScore ) {
+				${$file} = $score;
+			}
+		}
+	}
+	
+	my @sqlFiles = sort { $files{$a} <=> $files{$b} } keys %files;
+	
+	return @sqlFiles;
+}
+
+
+
+
+
 
 1;
