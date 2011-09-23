1390d1cd891e3a65e223bf51f8c94a7bc320c5be
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-09-23 02:22:12 +0000

    Added installation dialogue.

--- installer/installer-lib/mxHero/ClamAV.pm
+++ installer/installer-lib/mxHero/ClamAV.pm
@@ -2,25 +2,60 @@ package mxHero::ClamAV;
 
 use strict;
 use warnings;
+no warnings qw(uninitialized);
+
+use Term::UI;
+use Term::ReadLine;
 
 sub download
 {
-	return;
+	return 1;
 }
 
 sub install
 {
-	return;
+	# Note: if machine already has ClamAV use existing instance.
+	
+	if ( &_zimbraCheck ) {
+		# If has Zimbra - ask to use Zimbra ClamAV instance
+		print "*** NOTE FOR ZIMBRA INSTALLATIONS ***\n\n";
+		print "mxHero can use the ClamAV (antivirus) service provided by your Zimbra installation.\n";
+		my $term = Term::ReadLine->new( 'mxHero' );
+		my $bool = $term->ask_yn( prompt   => "Have mxHero use Zimbra's ClamAV service:",
+							   default  => 'y' );
+		if ( $bool ) {
+			# TODO - set mxHero to use Zimbra's ClamAV
+		} else {
+			# TODO - call standard installation
+		}
+	} else {
+		# TODO - call standard installation
+	}
+
+	
+	return 1;
 }
 
 sub upgrade
 {
-	return;
+	return 1;
 }
 
 sub configure
 {
-	return;
+	return 1;
 }
+
+## PRIVATE
+
+sub _zimbraCheck
+{
+	return 0 if ! -d "/opt/zimbra";
+
+	return 1;
+}
+
+
+
 
 1;
--- installer/installer-lib/mxHero/SpamAssassin.pm
+++ installer/installer-lib/mxHero/SpamAssassin.pm
@@ -5,22 +5,48 @@ use warnings;
 
 sub download
 {
-	return;
+	return 1;
 }
 
 sub install
 {
-	return;
+	
+	if ( &_zimbraCheck ) {
+		print "*** NOTE FOR ZIMBRA INSTALLATIONS ***\n\n";
+		print "Disable Zimbra's SpamAssassin once mxHero is up and running.\n";
+		print "This can be accomplished by running the command: <zimbra command>\n"; # TODO - get zimbra command
+		print "Hit <enter> to continue...\n";
+		while ( <STDIN> ) { # wait for enter
+			last;
+		}
+	}
+	
+	# TODO - install SpamAssassin
+	# in future add dialogue for external SpamAssassin (ex. get IP and Port)
+	
+	return 1;
 }
 
 sub upgrade
 {
-	return;
+	return 1;
 }
 
 sub configure
 {
-	return;
+	return 1;
 }
+
+## PRIVATE
+
+sub _zimbraCheck
+{
+	return 0 if ! -d "/opt/zimbra";
+
+	return 1;
+}
+
+
+
 
 1;
