822d063a555a467cbce88105fde27ad2abcef8a9
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-10-11 00:36:54 +0000

    Copy counter.

--- installer/installer-lib/mxHero/JDK.pm
+++ installer/installer-lib/mxHero/JDK.pm
@@ -6,6 +6,10 @@ use strict;
 use warnings;
 no warnings qw(uninitialized);
 
+use File::Path qw(make_path);
+use File::Find;
+use File::Copy;
+
 use mxHero::Config;
 use mxHero::Locale;
 
@@ -25,11 +29,15 @@ sub install
 	}
 
 	print T("Installing JDK... (takes time)"), "\n";
-	if ((system ("cp -a $myConfig{INSTALLER_PATH}/binaries/$dirName $myConfig{MXHERO_PATH}/$dirName")) != 0)
-	{
-		$$errorRef = T("Failed to copy java files");
-                return 0;
-	}
+	chdir "$myConfig{INSTALLER_PATH}/binaries";
+	&_progressCopy( "$dirName", "$myConfig{MXHERO_PATH}/$dirName" );
+	chdir "$myConfig{INSTALLER_PATH}";
+
+	# if ((system ("cp -a $myConfig{INSTALLER_PATH}/binaries/$dirName $myConfig{MXHERO_PATH}/$dirName")) != 0)
+	# {
+		# $$errorRef = T("Failed to copy java files");
+                # return 0;
+	# }
 
 	if (! symlink ("$myConfig{MXHERO_PATH}/$dirName", "$myConfig{MXHERO_PATH}/java"))
 	{
@@ -60,6 +68,38 @@ sub configure
 	my $errorRef = $_[0];
 	
 	return 1;
+}
+
+my $progressCopyDir; # destination
+sub _progressCopy
+{
+	my $dir1 = $_[0];
+	$progressCopyDir = $_[1];
+	
+	my @dirs;
+	push( @dirs, $dir1 );
+	find( {'wanted' => \&wanted, 'no_chdir' => 1, bydepth => 0}, @dirs );
+	print "\nDone\n";
+}
+
+my $count = 0;
+sub wanted
+{
+	my $dir = $progressCopyDir;
+	if ( -d $File::Find::name ) {
+		if  ( ! -d "$dir/$File::Find::name" ) {
+			make_path( "$dir/$File::Find::name" );
+		}
+	} else { # is file
+		if ( ! -d "$dir/$File::Find::dir" ) {
+			make_path( "$dir/$File::Find::dir" );
+		}
+		if ( ! copy( $File::Find::name, "$dir/$File::Find::name" ) ) {
+			warn "Copy failed: $!\n$File::Find::name => $dir/$File::Find::name\nExiting installation\n";
+			exit 1;
+		}
+		print "Files: ".$count++."\r";
+	}
 }
 
 1;
