a0dc6cc7b6f0e161a8077a1950e186e22d6bf394
apanagides <apanagides@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-11-30 14:35:30 +0000

    New file for Hero Attach configuration.

---
+++ installer/installer-lib/mxHero/HeroAttach.pm
@@ -0,0 +1,61 @@
+package mxHero::HeroAttach;
+
+use strict;
+use warnings;
+no warnings qw(uninitialized);
+
+use Term::UI;
+use Term::ReadLine;
+
+use mxHero::Config;
+use mxHero::Locale;
+
+my $defaultMax = 256; # in MB
+
+sub install
+{
+	my $errorRef = $_[0];
+	
+	my $term = Term::ReadLine->new( 'mxHero' );
+	my $reply;
+	
+	$reply = $term->get_reply( prompt => T("What is the maximum size email in MegaBytes to allow for Hero Attach")." [$defaultMax] ",
+				   default  => $defaultMax );
+	
+	$reply =~ s/[^\d]*(\d+)[^\d]*/$1/g; # Extract only numbers in case user adds MB to input.
+	
+	print T( "\nEnsure that your users can send emails to your email server of up to" ) . "$defaultMax MB\n";
+	print T( "Enter to continue" ) . "...\n";
+	my $enter = <STDIN>;
+	
+	## ALTER main.cf established in Postfix.pm around lines 63-99
+	### NEED TO EXPORT / GLOBAL VALUE IN Postfix.pm
+	
+	## Discover IP
+	my $ip = '0.0.0.0'; # IP DISCOVERY HERE
+	$reply = $term->get_reply(
+					prompt => T("What is the external address or IP of this mxHero installation. Hit enter to use the auto-detected ip address ")." [$ip] ",
+					default  => $ip );
+	
+	# SET IP ADDRESS for HERO ATTACH IN MXHERO
+	
+	return 1;
+}
+
+
+sub upgrade
+{
+	my $errorRef = $_[0];
+	
+	return 1;
+}
+
+
+sub configure
+{
+	my $errorRef = $_[0];
+
+	return 1;
+}
+
+1;
