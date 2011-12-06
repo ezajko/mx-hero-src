171e62edc703dfcee81c17723ff7198c7ddd7b5f
bcsantos <bcsantos@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2011-12-06 01:35:22 +0000

    fixes

--- installer/installer-lib/mxHero/Config.pm
+++ installer/installer-lib/mxHero/Config.pm
@@ -21,7 +21,7 @@ our %myConfig;
 
 $myConfig{JDK_X86_DIRNAME} = 'jdk1.6.0_29-i586';
 $myConfig{JDK_X64_DIRNAME} = 'jdk1.6.0_29-x64';
-$myConfig{MXHERO_INSTALL_VERSION} = '1.1.0.RELEASE';
+$myConfig{MXHERO_INSTALL_VERSION} = '1.2.0.RELEASE';
 $myConfig{RPMFORGE_BASE_URL} = "http://packages.sw.be/rpmforge-release";
 $myConfig{RPMFORGE_EL6_X86} = 'rpmforge-release-0.5.2-2.el6.rf.i686';
 $myConfig{RPMFORGE_EL6_X64} = 'rpmforge-release-0.5.2-2.el6.rf.x86_64';
@@ -31,5 +31,8 @@ $myConfig{RPMFORGE_EL6_X64} = 'rpmforge-release-0.5.2-2.el6.rf.x86_64';
 $myConfig{INSTALLER_PATH} = dirname($0);
 $myConfig{MXHERO_PATH} = '/opt/mxhero';
 $myConfig{TOMCAT_WEBAPPS_PATH} = '/var/lib/tomcat6/webapps';
+$myConfig{MXHERO_POSTFIX_CONFIG} = $myConfig{MXHERO_PATH} . '/configuration/properties/org.mxhero.engine.plugin.postfixconnector.cfg';
+$myConfig{MXHERO_HEROATTACH_CONFIG} = $myConfig{MXHERO_PATH} . '/configuration/properties/org.mxhero.engine.plugin.attachmentlink.cfg';
+$myConfig{CURRENT_POSTFIX_MAIN_CF} = '/etc/postfix/main.cf'; # only a default value
 
 1;
