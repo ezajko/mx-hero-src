384874900a89be60d2f8772050f84483fb2ca7a9
bcsantos <bcsantos@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2012-06-05 22:19:19 +0000

    fixes

--- installer/conf/postfix/transports.sql
+++ installer/conf/postfix/transports.sql
@@ -2,4 +2,4 @@ hosts=127.0.0.1
 user=mxhero
 password=mxhero
 dbname=mxhero
-query=SELECT DISTINCT CONCAT('smtp:[',server,']') FROM domain d LEFT JOIN domains_aliases a ON (d.domain = a.domain) WHERE alias = '%d'
+query=SELECT DISTINCT CONCAT('smtp:[', SUBSTRING_INDEX(server, ':', 1),']:', IF(LOCATE(':', server), SUBSTRING_INDEX(server, ':', -1), 25)) FROM domain d LEFT JOIN domains_aliases a ON (d.domain = a.domain) WHERE alias = '%d'
