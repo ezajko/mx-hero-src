ac5c740469a0416283ea7077f5d93f316f4550b0
jproyo <jproyo@badd6ff7-441d-4e21-a60f-5cb7f8d85ca5>
2012-09-10 18:15:24 +0000

    Ignoring integration test

--- trunk/org.mxhero.engine.plugin/boxstorage/src/test/java/org/mxhero/engine/plugin/boxstorage/internal/sinchro/SynchronizerTest.java
+++ trunk/org.mxhero.engine.plugin/boxstorage/src/test/java/org/mxhero/engine/plugin/boxstorage/internal/sinchro/SynchronizerTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@Ignore
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:sinchro_test.xml"})
 public class SynchronizerTest {
